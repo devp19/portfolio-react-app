c8c9496784617fb2749b51f7614f72214544c250
Dev Patel <devptl841806@gmail.com>
2024-08-23 01:08:32 -0400

    routing update for net

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import Home from './components/pages/Home'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './components/pages/Contact';
+import Success from './components/pages/Success';  // Adjust the path as necessary
+import Home from './components/pages/Home';
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-    
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/success" element={<Success />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
