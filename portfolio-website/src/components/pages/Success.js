7c50845a69bd7980ab9c0a63d7df0ef8208dc95e
Dev Patel <devptl841806@gmail.com>
2024-08-23 00:50:50 -0400

    form submission success

---
+++ portfolio-website/src/components/pages/Success.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Home(){
+    return(
+
+        <div>
+            <div className="background">
+                <div className='row'>
+                    <div className="col-md border full center-container">
+                        <div className="centered-text">
+                            <h5 className="supporting">Success!</h5>
+                            <h2 className="title">Form Submitted!</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
