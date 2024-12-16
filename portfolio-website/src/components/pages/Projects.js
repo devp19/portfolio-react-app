307b75124b985fccfbdd4988777033e609cd8c4e
Dev Patel <devptl841806@gmail.com>
2024-12-16 03:34:52 -0500

    .

--- portfolio-website/src/components/pages/Projects.js
+++ portfolio-website/src/components/pages/Projects.js
@@ -13,26 +13,33 @@ const Projects = forwardRef((props, ref) => {
 
                     <div className="row justify-content-center left-sep">
                         {/* Project I */}
-                        <div className="col-md-5 left-sep bottom-border">
-                            <div className="title-3">I. ResDex <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16">
+                        <div className="col-md-5 left-sep bottom-border pt-4">
+                            <div className="title-3">ResDex <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-folder-fill" viewBox="0 0 16 16">
   <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z"/>
 </svg> </div>
             
-                            <div className="para-text pt-4">
-                                <strong> Research Sharing Platform</strong>
+                            <div className="para-text pt-2">
+                                <i style={{borderLeft: "3px solid black"}}><strong style={{marginLeft: '10px'}}>Full-Stack Web Application</strong></i>
                                 <br /><br />
-                                • Led a cross-functional team of medical students and developers to pitch the platform to prestigious institutions (UofT, McMaster, UOttawa), resulting in strategic partnerships and 500+ pre-signups.                                
-                                <br /><br />
-                                • Engineered integration of AWS (Cognito, S3) and Firebase (Auth, Firestore), enhancing security with AES-256 encryption and TLS 1.2, leading to a 40% reduction in vulnerabilities and a 35% increase in data retrieval speed. 
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Lead a cross-functional team of medical and engineering students to develop and pitch the platform to
+prestigious institutions (UofT, McMaster, UWaterloo), leading to 500+ pre-signups. </p></p>                                
+                            
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Built a network of expertise consisting of 12 PhD professionals across diverse fields, that ignited innovative
+insights and accelerated platform development. </p></p>                                
+                            
 
-                                <br /><br />
-                                • Automated backend workflows using AWS Lambda and Firebase Functions, removing the need for 6 manual processes and increasing system throughput to handle 1,500 additional requests per day.
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Utilized AWS (Cognito, S3) and Firebase (Auth, Firestore) for efficient user profiling and storage, while
+implementing caching strategies to minimize unnecessary API fetch calls, resulting in an on-average 30.20 ms
+improvement in profile loading times.</p></p>                                
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Automated backend workflows using AWS Lambda and Firebase Functions, leveraging serverless
+architecture to enhance scalability, enabling the handling of 1,500 additional requests per day.</p></p>                                
 
-                                <br /><br />
-                                • Collaborated with 10+ PhD professionals across diverse fields, building a network of expertise that ignited innovative insights and accelerated platform development.
-                                <br /><br />
 
-                                ↓ <a href="https://github.com/devp19/NEAR-Fantasy" target='_blank' rel="noreferrer">
+                            
+                                ↓ <a href="https://resdex.vercel.app/" target='_blank' rel="noreferrer">
+                                    <button className="custom-btn">View Platform ↗︎</button>
+                                </a>
+                                <a href="https://github.com/devp19/resdex/" target='_blank' rel="noreferrer">
                                     <button className="custom-btn">View on GitHub ↗︎</button>
                                 </a>
                             </div>
@@ -48,10 +55,10 @@ const Projects = forwardRef((props, ref) => {
                                 </div>
                                 <div className="col-md">
                                     <div className="para-text pt-4">
-                                        <span className="bold">Encryption</span>
+                                        <span className="bold">Partnerships</span>
                                         <br /><br />
                                         <div className="custom pb-4">
-                                           AES-256
+                                           McMaster
                                         </div>
                                     </div>
                                 </div>
@@ -60,21 +67,22 @@ const Projects = forwardRef((props, ref) => {
 
                         {/* Project II */}
                         <div className="col-md-5 left-sep bottom-border pt-4">
-                            <div className="title-3">II. MyBuddy <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-journal-bookmark-fill" viewBox="0 0 16 16">
+                            <div className="title-3">MyBuddy <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-journal-bookmark-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6 1h6v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8z"/>
   <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
   <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
 </svg></div>
                             <div className="para-text">
-                                <strong>Mental Health Journaling Assistant</strong>
+                            <i style={{borderLeft: "3px solid black"}}><strong style={{marginLeft: '10px'}}>Generative AI Therapist –– SerenityHacks</strong></i>
                                 <br /><br />
-                                • Achieved 92% accuracy in therapeutic conversation similarity using 100+ prompts by employing
-prompt-engineering techniques to train and fine-tune the model for active listening.
-                                <br /><br />
-                                • Automated real-time responses by integrating Google Cloud’s Speech-to-Text API with OpenAI’s GPT
-Model, resulting in accurate transcriptions and response times in under 100ms in conversational applications.
-                                <br /><br />
-                                • Ranked 2nd in the best open-source project category at SerenityHacks and received a 9.72/10 user satisfaction rating over a minimum 2-week journaling period.                                <br /><br />
+
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Achieved 92% accuracy in therapeutic conversation similarity using 100+ prompts by employing
+prompt-engineering techniques to train and fine-tune the model for active listening.</p></p>
+
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Automated real-time responses by integrating Google Cloud’s Speech-to-Text API with OpenAI’s LLM, resulting in accurate transcriptions and response times in under 100ms in conversational applications.</p></p>                                
+                                
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}>                                 Ranked 2nd in the best open-source project category at SerenityHacks and received a 9.72/10 user satisfaction rating over a minimum 2-week journaling period.</p></p>                                
+                            
                                 ↓ <a href="https://github.com/devp19/MyBuddy" target='_blank' rel="noreferrer">
                                     <button className="custom-btn">View on GitHub ↗︎</button>
                                 </a>
@@ -91,7 +99,7 @@ Model, resulting in accurate transcriptions and response times in under 100ms in
                                 </div>
                                 <div className="col-md">
                                     <div className="para-text pt-4">
-                                        <span className="bold">Best Open Source Project</span>
+                                        <span className="bold">Best Open Source Project Rank</span>
                                         <br /><br />
                                         <div className="custom pb-4">
                                             No. 2
@@ -103,20 +111,19 @@ Model, resulting in accurate transcriptions and response times in under 100ms in
 
                         {/* Project III */}
                         <div className="col-md-5 left-sep bottom-border pt-4">
-                            <div className="title-3">III. NEAR Fantasy <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                            <div className="title-3">NEAR Fantasy <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/>
 </svg></div>
             
                             <div className="para-text pt-4">
-                                <strong> Fantasy Sports Platform Built On The NEAR Blockchain</strong>
+                            <i style={{borderLeft: "3px solid black"}}><strong style={{marginLeft: '10px'}}>Fantasy-Sports Prototype on the NEAR Blockchain</strong></i>
                                 <br /><br />
-                                • Improved transaction security by 40% through the creation of a secure and private payment portal utilizing the
-NEAR Blockchain, renowned for its robust security features, scalability, and decentralized architecture.                                <br /><br />
-                                • Projected to save over $10,000 in financial resources towards NFT-digital creations when compared to traditional
-methods by leveraging the DALL-E ecosystem for instant cost-free additions to the collection.                                
-<br /><br />
-• Labelled as the 1st ever fantasy-sports platform created on the NEAR Blockchain at HawkHacks.
-<br /><br />
+
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Improved transaction security by 40% through the creation of a secure and private payment portal utilizing the
+NEAR Blockchain, renowned for its robust security features, scalability, and decentralized architecture. </p></p>                                
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Projected to save over $10,000 in financial resources towards NFT-digital creations when compared to traditional
+methods by leveraging the DALL-E ecosystem for instant cost-free additions to the collection. </p></p>                                
+                                <p style={{borderLeft: "3px solid black"}}>  <p style={{marginLeft: '10px'}}> Labelled as the 1st ever fantasy-sports platform created on the NEAR Blockchain at HawkHacks. </p></p>                                
 
                                 ↓ <a href="https://github.com/devp19/NEAR-Fantasy" target='_blank' rel="noreferrer">
                                     <button className="custom-btn">View on GitHub ↗︎</button>
