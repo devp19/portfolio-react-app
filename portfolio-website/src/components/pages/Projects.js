bf816563c6fec7682ba26fda38fbc79d6129f9ff
Dev Patel <devptl841806@gmail.com>
2025-05-18 21:58:07 -0400

    resdex img update

--- portfolio-website/src/components/pages/Projects.js
+++ portfolio-website/src/components/pages/Projects.js
@@ -3,7 +3,7 @@ import QonnectR from '../images/qr.png';
 import Resdex from '../images/resdex1.png';
 import MyBuddy from '../images/MyBuddy.png';
 import QonnectRWin from '../images/win2.png';
-
+import ResDexShow from '../images/resdex-phone-2.png';
 const Projects = forwardRef((props, ref) =>
      {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -30,56 +30,6 @@ const Projects = forwardRef((props, ref) =>
           <div className="title-2 text-center mt-4">Projects</div>
 
           <div className='col-md-9 p-3'>
-            <div className='row mt-3 box d-flex align-items-center' style={{ marginLeft: '20px', marginRight: '20px' }}>
-              
-              <div className='col-auto d-flex align-items-center justify-content-center'>
-                <img
-                  className='img-fluid'
-                  src={QonnectR}
-                  alt='QonnectR'
-                  style={{ maxHeight: '50px', borderRadius: '50%' }}
-                />
-              </div>
-
-              <div className='col'>
-                <div className='supporting-2 d-flex change-font'>
-                  QonnectR
-                </div>
-                <div className='text-muted d-flex' style={{ fontSize: '10px' }}>
-                  <i>DeltaHacks XI Winner (January, 2025)</i>
-                </div>
-              </div>
-
-              <div className='col-auto d-flex justify-content-end'>
-                <a onClick={handleReadMoreClick} style={{ cursor: 'pointer', fontSize: '12px' }} className='text-muted'>
-                  <i>[ {isExpanded ? 'Read Less' : 'Read More'} ]</i>
-                </a>
-              </div>
-
-              {isExpanded && (
-                <div className='row mt-3 description' style={{ marginLeft: '20px', marginRight: '20px' }}>
-                  <div className='col'>
-                    <p className='text-muted'>
-                    Have you ever found it hard to come up with inspiring project ideas, or struggled to stay motivated once you've started? Trust me, I’ve been there too. That’s exactly why we built QonnectR. It’s a platform designed to make collaboration seamless and exciting. You can create a profile, showcase your projects, and invite others to join your journey. But it doesn’t stop there—QonnectR also lets you discover and connect with passionate people, so you can jump into projects that excite you. Think of it as a space where solo ideas transform into dynamic team efforts. QonnectR isn’t just a tool—it’s a community where creativity meets collaboration, helping turn your visions into reality.
-
-</p>
-<img src={QonnectRWin} className='border-black' style={{height: '200px', padding: '10px', borderRadius: '5px'}}></img>
-<br></br>
-<br></br>
-<button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://qonnectr.vercel.app/', '_blank', 'noopener noreferrer')}>
-  Website ↗︎   
-</button>  
-<button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://devpost.com/software/qonnectr', '_blank', 'noopener noreferrer')}>
-  Devpost ↗︎   
-</button>  
-<button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://github.com/devp19/qonnectr', '_blank', 'noopener noreferrer')}>
-  GitHub ↗︎   
-</button>  
-
-                </div>
-                </div>
-              )}
-            </div>
 
 
             <div className='row mt-3 box d-flex align-items-center' style={{ marginLeft: '20px', marginRight: '20px' }}>
@@ -116,6 +66,10 @@ const Projects = forwardRef((props, ref) =>
 <br></br>
 <br></br>
 By engaging in peer and expert reviews, students can refine their work, gain valuable feedback, and build meaningful connections with academics and professionals in their field.                    </p>
+<br></br>
+<img src={ResDexShow} className='img-fluid border-black' style={{width: '400px', borderRadius: '5px'}}></img>
+<br></br>
+<br></br>
 <button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://resdex.vercel.app/', '_blank', 'noopener noreferrer')}>
   Website ↗︎   
 </button>  
@@ -128,6 +82,57 @@ By engaging in peer and expert reviews, students can refine their work, gain val
                 
 
                 
+              )}
+            </div>
+            
+            <div className='row mt-3 box d-flex align-items-center' style={{ marginLeft: '20px', marginRight: '20px' }}>
+              
+              <div className='col-auto d-flex align-items-center justify-content-center'>
+                <img
+                  className='img-fluid'
+                  src={QonnectR}
+                  alt='QonnectR'
+                  style={{ maxHeight: '50px', borderRadius: '50%' }}
+                />
+              </div>
+
+              <div className='col'>
+                <div className='supporting-2 d-flex change-font'>
+                  QonnectR
+                </div>
+                <div className='text-muted d-flex' style={{ fontSize: '10px' }}>
+                  <i>DeltaHacks XI Winner (January, 2025)</i>
+                </div>
+              </div>
+
+              <div className='col-auto d-flex justify-content-end'>
+                <a onClick={handleReadMoreClick} style={{ cursor: 'pointer', fontSize: '12px' }} className='text-muted'>
+                  <i>[ {isExpanded ? 'Read Less' : 'Read More'} ]</i>
+                </a>
+              </div>
+
+              {isExpanded && (
+                <div className='row mt-3 description' style={{ marginLeft: '20px', marginRight: '20px' }}>
+                  <div className='col'>
+                    <p className='text-muted'>
+                    Have you ever found it hard to come up with inspiring project ideas, or struggled to stay motivated once you've started? Trust me, I’ve been there too. That’s exactly why we built QonnectR. It’s a platform designed to make collaboration seamless and exciting. You can create a profile, showcase your projects, and invite others to join your journey. But it doesn’t stop there—QonnectR also lets you discover and connect with passionate people, so you can jump into projects that excite you. Think of it as a space where solo ideas transform into dynamic team efforts. QonnectR isn’t just a tool—it’s a community where creativity meets collaboration, helping turn your visions into reality.
+
+</p>
+<img src={QonnectRWin} className='border-black' style={{width: '400px', borderRadius: '5px'}}></img>
+<br></br>
+<br></br>
+<button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://qonnectr.vercel.app/', '_blank', 'noopener noreferrer')}>
+  Website ↗︎   
+</button>  
+<button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://devpost.com/software/qonnectr', '_blank', 'noopener noreferrer')}>
+  Devpost ↗︎   
+</button>  
+<button className="custom-btn mb-2 pl-2" onClick={() => window.open('https://github.com/devp19/qonnectr', '_blank', 'noopener noreferrer')}>
+  GitHub ↗︎   
+</button>  
+
+                </div>
+                </div>
               )}
             </div>
 
